deb00d4b7dd409f2f6d30efa7ffe676716aa5f88
中川亮 <nakagawaryou@nakagawaryounoMacBook-Air.local>
2025-07-20 18:56:47 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('drawing', (data) => {
+    socket.broadcast.emit('drawing', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
