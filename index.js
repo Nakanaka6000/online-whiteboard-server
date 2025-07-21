cae0aefa21bb7d81237dfb64bf6e635406a38d0e
中川亮 <nakagawaryou@nakagawaryounoMacBook-Air.local>
2025-07-22 06:37:00 +0900

    feat: implement server-side state management

--- index.js
+++ index.js
@@ -11,31 +11,57 @@ const io = socketIo(server, {
   }
 });
 
+let drawnElements = []; // Stores the current state of the whiteboard
+
 io.on('connection', (socket) => {
   console.log('a user connected');
 
+  // Send current state to the newly connected client
+  socket.emit('initialState', drawnElements);
+
   // Drawing event
   socket.on('drawing', (data) => {
+    // Update server state (for paths, we need to reconstruct them)
+    if (data.type === 'start') {
+      socket.currentPath = [data];
+    } else if (data.type === 'draw') {
+      if (socket.currentPath) socket.currentPath.push(data);
+    } else if (data.type === 'stop') {
+      if (socket.currentPath) {
+        drawnElements.push({ type: 'path', path: socket.currentPath, strokeColor: data.strokeColor, strokeWidth: data.strokeWidth });
+        socket.currentPath = [];
+      }
+    }
     socket.broadcast.emit('drawing', data);
   });
 
   // Clear event
   socket.on('clear', () => {
+    drawnElements = []; // Clear server state
     io.emit('clear');
   });
 
   // Image event
   socket.on('image', (imageData) => {
+    drawnElements.push(imageData); // Add image to server state
     socket.broadcast.emit('image', imageData);
   });
 
   // Element update event (move/resize)
   socket.on('elementUpdate', (updateData) => {
+    const elementToUpdate = drawnElements.find(el => el.id === updateData.id);
+    if (elementToUpdate) {
+      elementToUpdate.x = updateData.x;
+      elementToUpdate.y = updateData.y;
+      if (updateData.width !== undefined) elementToUpdate.width = updateData.width;
+      if (updateData.height !== undefined) elementToUpdate.height = updateData.height;
+    }
     socket.broadcast.emit('elementUpdate', updateData);
   });
 
   // Text event
   socket.on('text', (textData) => {
+    drawnElements.push(textData); // Add text to server state
     socket.broadcast.emit('text', textData);
   });
 
